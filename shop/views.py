e9a8724c90440feb4b2d040980c5d354dff0bd14
USER1 <emoleg2008@gmail.com>
2024-03-13 19:52:15 +0500

    My project

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views.generic import DetailView
+
 from shop.models import Category, Product
 
 
@@ -10,6 +12,10 @@ def category_list(request):
     return render(request, 'shop/category_list.html', context)
 
 
+# def category_detail(request, pk):
+#     detail = Category.objects.filter(pk=pk)
+#
+
 class Category_detail(DetailView):
     model = Category
     template_name = 'shop/category_detail.html'
@@ -17,14 +23,14 @@ class Category_detail(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        print(self.object)
         products = Product.objects.filter(category=self.object)
         context['products'] = products
         return context
 
 
-def add_to_cart(request, product_id, category_id):
-    # request.session['cart'] = []
+def add_to_cart(request, product_id):
+    if not request.session.get('cart'):
+        request.session['cart'] = []
     cart = request.session['cart']
     items = [i['product'] for i in cart]
     if product_id in items:
@@ -33,24 +39,28 @@ def add_to_cart(request, product_id, category_id):
                 i['quantity'] += 1
                 break
     else:
-        cart_item = {'product': product_id, 'quantity': 1}
-    cart.append(1)
-    request.session.modifid = True
+        cart_item = {
+            "product": product_id,
+            "quantity": 1
+        }
+        cart.append(cart_item)
+
+    request.session.modified = True
     product = Product.objects.get(pk=product_id)
-    category = product.category.pk
+    category_id = product.category.pk
     print(cart)
     return redirect('category_detail', category_id)
 
 
 def cart(request):
-    my_cart = request.session.get('cart')
+    my_cart = request.session.get('cart', [])
     my_cart_context = []
-    for item in my_cart:
+    for item in my_cart:  # {'product': 1, 'quantity': 8}
         my_cart_item = {}
         my_cart_item['product'] = Product.objects.get(pk=item['product'])
+        my_cart_item['quantity'] = item['quantity']
+        my_cart_item['total'] = float(my_cart_item['product'].price * my_cart_item['quantity'])
         my_cart_context.append(my_cart_item)
 
-    print(my_cart_item)
-    context = {'cart': cart}
+    context = {'cart_items': my_cart_context}
     return render(request, 'shop/cart.html', context)
-
